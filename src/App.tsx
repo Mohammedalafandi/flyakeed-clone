9bffcf646144ae769cd2cf01ab221b0ad273ff0a
ViaMyo <viamyo.it@gmail.com>
2023-11-20 12:52:33 -0600

    added the search flight

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Footer from './components/footer'
-import LandingPage from './components/Landingpage'
-import SearchFlight from './components/searchpage'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import LandingPage from "./components/landingPage";
+import SearchFlight from "./components/searchpage";
+
 function App() {
- 
-
   return (
-    <div className='app'>
-
-    
-    
-   <BrowserRouter>
-   <Navbar/>
-   <Routes>
-    <Route path='/' element={<LandingPage/>}/>
-    <Route path='search' element={<SearchFlight/>}/>
-   </Routes>
-   <Footer />
-   </BrowserRouter>
-   </div>
- 
-   
-    
-     
-   
-  )
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="search" element={<SearchFlight />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
